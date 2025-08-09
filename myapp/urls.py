72e5704c4cd0f6a10837498e4907a05d38ba86e8
VIKAS YADAV <mr.xyzusername@gmail.com>
2025-08-09 14:41:21 +0530

    updated

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,15 @@
+
 from django.urls import path
 from .import views
 urlpatterns = [
-path('',views.home,name='home'),
+ path('',views.home,name='home'),
 path('about/',views.about,name='about'),
-path('services/',views.services,name='services'),
-path('languages/',views.languages,name='languages'),
+path('add/', views.add_blog, name='add_blog'),
+path('dashboard/',views.dashboard,name='dashboard'),
 path('contact/',views.contact,name='contact'),
 path('signuppage/',views.signuppage,name='signuppage'),
 path('loginpage/',views.loginpage,name='loginpage'),
+path('thank/',views.thank, name='thank'),
+path('logoutpage/',views.logoutpage, name='logoutpage'),
 path('readmore/<int:blog_id>',views.readmore,name='readmore'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 # Create your views here.
 def home(request):
     ap=Post.objects.all
@@ -7,16 +9,46 @@ def home(request):
     return render(request,'myapp/home.html',{'ap':ap})
 def about(request):
     return render(request,'myapp/about.html')
-def services(request):
-    return render(request,'myapp/services.html')
-def languages(request):
-    return render(request,'myapp/languages.html')
+
+def dashboard(request):
+    return render(request,'myapp/dashboard.html')
 def contact(request):
     return render(request,'myapp/contact.html')
 def signuppage(request):
-    return render(request,'myapp/signuppage.html')
+    if request.method == "POST":
+        f = UserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            return redirect('thank')
+    else:
+        f = UserCreationForm()
+
+
+
+    return render(request,'myapp/signuppage.html',{'f': f})
 def loginpage(request):
-    return render(request,'myapp/loginpage.html')
+    if request.method == 'POST':
+        f = AuthenticationForm(request, data=request.POST)
+        if f.is_valid():
+            un = f.cleaned_data.get('username')
+            pd = f.cleaned_data.get('password')
+            m = authenticate(username=un, password=pd)
+            if m is not None:
+                login(request, m)
+                return redirect('dashboard')
+    else:
+        f = AuthenticationForm()
+    return render(request, 'myapp/loginpage.html', {'f': f})
+
 def readmore(request,blog_id):
     n = get_object_or_404(Post, id=blog_id)
     return render(request,'myapp/readmore.html',{'n': n})
+def thank(request):
+    return render(request,'myapp/thank.html')
+def logoutpage(request):
+    logout(request)
+    return redirect('/')
+
+def add_blog(request):
+
+    return render(request, 'myapp/add_blog.html')
